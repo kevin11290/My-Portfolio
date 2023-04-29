9fbd9ff4dfd79c04a4a7d3e451e0d1f56ff4daf4
kevinsimpson1129@gmail.com <kevinsimpson11290@gmail.com>
2023-04-30 05:17:46 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <main>
+      <Navbar/>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Testimonials/>
+      <Contact/>
+    </main>
+  );
+}
+
+export default App;
