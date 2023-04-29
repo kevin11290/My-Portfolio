96dcd17ae901c0841de9651285bf725a6cb4de58
kevinsimpson1129@gmail.com <kevinsimpson11290@gmail.com>
2023-04-30 08:10:38 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Projects from './components/Proects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
